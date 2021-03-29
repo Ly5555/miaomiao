c687c9c459ef6e7b3f6484afd36b234fe8412b94
Ly5555 <862695473@qq.com>
2021-03-29 15:59:34 +0800

    dev

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import MovieRouter from './movie'
+import MineRouter from './mine'
+import CinemaRouter from './cinema'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    MovieRouter,
+    MineRouter,
+    CinemaRouter,
+    {
+      path : '/*',
+      redirect : '/movie'
+    }
+  ]
+})
+export default router
